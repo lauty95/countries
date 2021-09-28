165451cd942d561bc93d750a3edef70d1cc17983
lauty95 <lautaroJ95@gmail.com>
2021-09-27 21:07:23 -0300

    change api v2 to v3

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,17 +5,17 @@ const activities = require("./activities")
 const router = express();
 const axios = require("axios").default;
 
-axios.get("https://restcountries.com/v2/all")
+axios.get("https://restcountries.com/v3/all")
     .then(async response => {
         await Pais.sync({ force: true })
         response.data.map(async pais => {
-            if(pais.capital){
+            if(pais.flags && pais.capital){
                 await Pais.create({
-                    id: pais.alpha3Code,
-                    nombre: pais.name,
+                    id: pais.cca3,
+                    nombre: pais.name.common,
                     bandera: pais.flags[0],
-                    continente: pais.continent,
-                    capital: pais.capital,
+                    continente: pais.region,
+                    capital: pais.capital[0],
                     subregion: pais.region,
                     area: pais.area,
                     poblacion: pais.population
