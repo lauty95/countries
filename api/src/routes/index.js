eee4b89d1610d8025f6bb91edf8727c94cc9c6b7
lauty95 <lautaroJ95@gmail.com>
2021-10-02 18:39:30 -0300

    modified api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,41 +6,41 @@ const router = express();
 const axios = require("axios").default;
 const paises = require("./../countries.json")
 
-function cargarDB(pais) {
-    return {
-        id: pais.alpha3Code,
-        nombre: pais.name,
-        bandera: pais.flag,
-        continente: pais.region,
-        capital: pais.capital,
-        subregion: pais.region,
-        area: pais.area,
-        poblacion: pais.population,
-    }
-}
+// function cargarDB(pais) {
+//     return {
+//         id: pais.alpha3Code,
+//         nombre: pais.name,
+//         bandera: pais.flag,
+//         continente: pais.region,
+//         capital: pais.capital,
+//         subregion: pais.region,
+//         area: pais.area,
+//         poblacion: pais.population,
+//     }
+// }
 
-// axios.get("https://restcountries.com/v2/all")
-//     .then(async response => {
-//         await Pais.sync({ force: false })
-//         response.data.map(async pais => {
-//             if(pais.flags && pais.capital){
-//                 await Pais.create({
-//                     id: pais.alpha3Code,
-//                     nombre: pais.name,
-//                     bandera: pais.flags.svg,
-//                     continente: pais.region,
-//                     capital: pais.capital,
-//                     subregion: pais.region,
-//                     area: pais.area,
-//                     poblacion: pais.population,
-//                 })
-//             }
-//         })
-//     });
-function fillDB () {
-    let response = paises.map(p => cargarDB(p))
-    return Pais.create(response);
-}
+axios.get("https://restcountries.com/v2/all")
+    .then(async response => {
+        await Pais.sync({ force: false })
+        response.data.map(async pais => {
+            if(pais.flags && pais.capital){
+                await Pais.create({
+                    id: pais.alpha3Code,
+                    nombre: pais.name,
+                    bandera: pais.flags.svg,
+                    continente: pais.region,
+                    capital: pais.capital,
+                    subregion: pais.region,
+                    area: pais.area,
+                    poblacion: pais.population,
+                })
+            }
+        })
+    });
+// function fillDB () {
+//     let response = paises.map(p => cargarDB(p))
+//     return Pais.create(response);
+// }
 
 router.use("/api", countries);
 router.use("/api", activities);
